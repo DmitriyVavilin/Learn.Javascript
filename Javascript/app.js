8c71f633ced0fb07cb344f4c8e35746ac239dcf1
DmitriyVavilin <dv.vavilin@mail.ru>
2022-02-28 15:08:28 +0300

    two__task

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,5 +1,19 @@
+/*Решение задачи 1
+==================*/
+
 let table = document.querySelector("body > div > div.after > table")
 for(let i = 0; i < table.rows.length; i++) {
     let row = table.rows[i]
     row.cells[i].style.background = 'red'
 }
+
+
+/*Решение задачи 2
+==================*/
+/*for(let li of document.querySelectorAll('li')) {
+    let title = li.firstChild.data
+    title = title.trim()
+    let input = li.getElementsByTagName('li').length
+    alert(title + ': ' + input)
+}*/
+
